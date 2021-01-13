f43be883ab998fc88d0cce98c82df9d37a9ef5bb
Anna Skorykh <egreth@mail.ru>
2021-01-13 21:39:11 +0300

    modified nginx.conf, init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 cd /home/box/web/
-gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8080 hello:app &
+cd ask/
+gunicorn -b 0.0.0.0:8000 ask.wsgi:application
