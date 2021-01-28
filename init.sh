ebb35e1e5f1fad30a59ec49148c14db4b27b7138
Anna Skorykh <egreth@mail.ru>
2021-01-28 17:55:44 +0300

    split init.sh into init.sh and setup.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,4 @@
-sudo apt update
-sudo apt install python3.5
-sudo apt install python3.5-dev
-sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo python3 -m pip install gunicorn
-sudo python3 -m pip install django==2.0
-sudo python3 -m pip install mysqlclient
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 cd /home/box/web/
