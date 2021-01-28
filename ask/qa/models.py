a5cf2aebfcf9da8531e4a460a7c87d07bd932e70
Anna Skorykh <egreth@mail.ru>
2021-01-28 19:05:51 +0300

    modified models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
-
-
 class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
@@ -19,9 +10,19 @@ class QuestionManager(models.Manager):
         return self.order_by('-rating')
 
 
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
