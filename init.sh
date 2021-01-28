f54cac840e643ae2d0f1a0c1302f1129175b689b
Anna Skorykh <egreth@mail.ru>
2021-01-28 15:55:56 +0300

    create MySQL database

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ cd /home/box/web/
 gunicorn -b 0.0.0.0:8080 hello:app &
 cd ask/
 gunicorn -b 0.0.0.0:8000 ask.wsgi:application
+sudo mysql -uroot -e "create database stepic_web;" # создание базы данных stepic_web
+sudo mysql -uroot -e "CREATE USER 'egreth'@'localhost' IDENTIFIED BY 'password';" # создание пользователя egreth с паролем password
+sudo mysql -uroot -e "GRANT ALL ON stepic_web.* TO 'egreth'@'localhost';" # выдача прав пользователю
